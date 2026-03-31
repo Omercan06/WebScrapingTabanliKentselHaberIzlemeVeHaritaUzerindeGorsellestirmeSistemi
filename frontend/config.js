b36f82bffccb088f68886d16e2c93e0547c5efd5
Ömercan Öğütlü <omer06ogutlu34@gmail.com>
2026-03-31 19:42:34 +0300

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     // Buraya Google Maps ve Geocoding servisleri için API Anahtarınızı giriniz.
     // Google Cloud, her ay 200$ (yaklaşık 100.000 harita yüklemesi) ücretsiz kredi verir.
     // Bu sayede üniversite projelerinde tamamen ücretsiz kullanılabilir.
-    GOOGLE_MAPS_API_KEY: "AIzaSyAXGfTq8HvLPKNKaXJvcpGmjT84FUO_Xi8",
+    GOOGLE_MAPS_API_KEY: "GoogleMaps API Key",
     
     // Backend API URL
     API_BASE_URL: "http://127.0.0.1:8000"
